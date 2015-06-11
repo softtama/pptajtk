2cee879bfaaae34e0f08d4b594508b770320b817
Rizki Pratama <m121511027@students.jtk.polban.ac.id>
2015-06-11 20:38:13 +0700

    major changes: added fixed left sidebar navmenu and other changes to some elements in v_mengelolaKelompokTA; added file c_mengelolaSeminarSatu

---
+++ application/controllers/c_mengelolaSeminarSatu.php
@@ -0,0 +1,16 @@
+<?php
+	class C_mengelolaSeminarSatu extends CI_controller
+	{
+		function _construct()
+		{
+			parent::_construct();
+		}
+		
+		public function index()
+		{
+			
+		}
+		
+		
+	}
+?>
